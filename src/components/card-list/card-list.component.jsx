8c3ce1f9eced5ebc3dafe62a6aac0ad21a0b7d16
julian-leahy <julian.leahy.dev@gmail.com>
2020-07-03 23:56:09 +1200

    style cards

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Card } from '../card/card.component';
 
+import './card-list.style.css';
+
 export const CardList = (props) => {
     return (
-        <div>
+        <div className='card-container'>
             {props.robots.map(robot => <Card key={robot.id} name={robot.name} id={robot.id} />)}
         </div>
     )
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './card.style.css';
+
 export const Card = (props) => {
     return (
-        <div>
+        <div className='card'>
             <img src={`https://robohash.org/${props.id}?set=set2&size=180x180`} alt="" />
             <h3>{props.name}</h3>
         </div>
